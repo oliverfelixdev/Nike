47b523d2d14fb8d4868613a54dae63e2078fc5e5
Oliver Felix <oliverfelix.dev@gmail.com>
2025-02-13 05:16:41 +0000

    Shoe Model Added
    Added floating, tilting, and subtle random movement for a more natural and dynamic feel.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,28 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Button from "./Button";
+import Shoemodel from "./Shoemodel";
 
 const Home = () => {
   return (
     <div className="home">
       <div className="home-container">
         <div className="home-content">
-          <h1 className="home-title">NIKE, JUST DO IT!</h1>
-          <p className="home-description subtitle-lg">
-            They'll talk whether you win or lose. So leave 'em speechless.
-          </p>
-          <p className="home-description subtitle-lg mb-2 sm:block hidden">
-            They'll talk whether you rise or fall. So leave 'em breathless.
-          </p>
-          <Link to="/newarrivals">
-            <Button />
-          </Link>
+          <div className="home-content-wrap">
+            <h1 className="home-title">NIKE, JUST DO IT!</h1>
+            <p className="home-description subtitle-lg">
+              They'll talk whether you win or lose. So leave 'em speechless.
+            </p>
+            <p className="home-description subtitle-lg mb-2 sm:block hidden">
+              They'll talk whether you rise or fall. So leave 'em breathless.
+            </p>
+            <Link to="/newarrivals">
+              <Button />
+            </Link>
+          </div>
         </div>
       </div>
-{/*       <img
+      {/*       <img
         src="https://mir-s3-cdn-cf.behance.net/project_modules/fs/c2430c200355795.6660da54a5778.jpg"
         alt="Not Found"
       /> */}
+      <Shoemodel />
     </div>
   );
 };
---
+++ src/components/Shoemodel.jsx
@@ -0,0 +1,50 @@
+import React, { useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { OrbitControls, useGLTF } from "@react-three/drei";
+import { Bloom, EffectComposer } from "@react-three/postprocessing";
+const Model = () => {
+  const { scene } = useGLTF("/shoepegasus.glb");
+  //   console.log(scene);
+  const modelRef = useRef();
+  useFrame(({ clock }) => {
+    const t = clock.getElapsedTime(); // Time-based animation
+
+    // Breathing effect (scaling up and down)
+    const scaleFactor = 1.5 + 0.05 * Math.sin(t * 0.5);
+    modelRef.current.scale.set(scaleFactor, scaleFactor, scaleFactor);
+
+    // Left-right tilt effect
+    modelRef.current.rotation.z = Math.PI / 1.6 + 0.1 * Math.sin(t * 0.5);
+
+    // Floating top-bottom movement
+    modelRef.current.position.y = 0.2 * Math.sin(t * 0.5);
+  });
+
+  return (
+    <primitive
+      ref={modelRef}
+      object={scene}
+      rotation={[-Math.PI / 2, 1, Math.PI / 1.6]}
+    />
+  );
+};
+
+const Shoemodel = () => {
+  return (
+    <div className="shoe-model">
+      <div className="glb-model-main">
+        <Canvas flat camera={{ fov: 35, position: [0, 0, 5] }}>
+          {/* <OrbitControls /> */}
+          <ambientLight />
+          <directionalLight position={[2, 2, 2]} />
+          <EffectComposer>
+            <Bloom luminanceThreshold={0} luminanceSmoothing={1.5} />
+          </EffectComposer>
+          <Model />
+        </Canvas>
+      </div>
+    </div>
+  );
+};
+
+export default Shoemodel;
